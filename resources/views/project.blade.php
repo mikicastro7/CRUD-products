2bec46306645606ce88b1277a646550b06786140
Miquel Castro <castro-miquel@hotmail.com>
2021-03-07 23:19:22 +0100

    Bootstrap modal for edit added with data inside

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -43,17 +43,54 @@
                         <td>{{$product->updated_at}}</td>
                         <td>
                             <button type="button" class="delete-btn btn btn-danger"><i class="fas fa-trash"></i></button>
-                            <button type="button" class="btn btn-warning"><i class="fas fa-edit"></i></button>
+                            <button type="button" class="edit-btn btn btn-warning" data-toggle="modal" data-target="#editModal"><i class="fas fa-edit"></i></button>
                         </td>
                     </tr>
                 @endforeach
             </tbody>
             </table>
+            <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Edit</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form id="add-product">
+                            <div class="form-row">
+                                <div class="form-group col-12">
+                                    <label for="name-modal">Name</label>
+                                    <input id="name-modal" type="text" class="form-control" placeholder="Product name">
+                                </div>
+                                <div class="form-group col-12">
+                                    <label for="price-modal">Price</label>
+                                    <input id="price-modal" type="number" class="form-control" placeholder="Product price">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="description-modal">Description</label>
+                                <textarea id="description-modal" class="form-control" rows="3"
+                                    placeholder="description"></textarea>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="edit-product-button btn btn-primary">Save changes</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </main>
     </div>
 </body>
 
 <script>
+
+
     $('.delete-btn').on('click', function(e){
         let productId = $(this).parent().parent().get(0).id;
 
@@ -109,7 +146,7 @@
             for (const key in formatedObject) {
                 addRow.append("<td>" + formatedObject[key] + "</td>");
             }
-            addRow.append('<td><button type="button" class="delete-btn btn btn-danger"><i class="fas fa-trash"></i></button> <button type="button" class="btn btn-warning"><i class="fas fa-edit"></i></button></td>');
+            addRow.append('<td><button type="button" class="delete-btn btn btn-danger"><i class="fas fa-trash"></i></button> <button type="button" class="edit-btn btn btn-warning" data-toggle="modal" data-target="#editModal"><i class="fas fa-edit"></i></button></td>');
             $("tbody").append(addRow);
 
             $('#name').val("");
@@ -117,4 +154,14 @@
             $('#price').val("");
         })
     });
+
+    $('table').delegate('.edit-btn', 'click', function(e){
+        const name = $(this).parent().siblings().eq(1).text();
+        const description = $(this).parent().siblings().eq(2).text();
+        const price =  $(this).parent().siblings().eq(3).text();
+
+        $("#name-modal").val(name);
+        $("#description-modal").val(description);
+        $("#price-modal").val(price);
+    })
 </script>
