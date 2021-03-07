935d426befe6da4f7305d7f7e5303dff50c35135
Miquel Castro <castro-miquel@hotmail.com>
2021-03-07 11:35:17 +0100

    Front-end form added

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -3,35 +3,52 @@
     <div class="container">
         <h1 class="text-center p-3">Products project</h1>
         <main>
-                <table class="table">
-                <thead>
+            <form action="post">
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="name">Name</label>
+                        <input id="name" type="text" class="form-control" placeholder="Product name">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="price">Price</label>
+                        <input id="price" type="number" class="form-control" placeholder="Product price">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="description">Description</label>
+                    <textarea id="description" class="form-control" rows="1" placeholder="description"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary mb-3">Submit</button>
+            </form>
+            <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Price</th>
+                <th scope="col">Created At</th>
+                <th scope="col">Updated At</th>
+                <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($products as $product)
                     <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Created At</th>
-                    <th scope="col">Updated At</th>
-                    <th scope="col">Actions</th>
+                        <td>{{$product->id}}</td>
+                        <td>{{$product->name}}</td>
+                        <td>{{$product->description}}</td>
+                        <td>{{$product->price}}</td>
+                        <td>{{$product->created_at}}</td>
+                        <td>{{$product->updated_at}}</td>
+                        <td>
+                            <button type="button" class="btn btn-warning"><i class="fas fa-trash"></i></button>
+                            <button type="button" class="btn btn-danger"><i class="fas fa-edit"></i></button>
+                        </td>
                     </tr>
-                </thead>
-                <tbody>
-                    @foreach ($products as $product)
-                        <tr>
-                            <td>{{$product->id}}</td>
-                            <td>{{$product->name}}</td>
-                            <td>{{$product->description}}</td>
-                            <td>{{$product->price}}</td>
-                            <td>{{$product->created_at}}</td>
-                            <td>{{$product->updated_at}}</td>
-                            <td>
-                                <button type="button" class="btn btn-warning"><i class="fas fa-trash"></i></button>
-                                <button type="button" class="btn btn-danger"><i class="fas fa-edit"></i></button>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-                </table>
+                @endforeach
+            </tbody>
+            </table>
         </main>
     </div>
 </body>
